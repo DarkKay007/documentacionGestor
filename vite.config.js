f8450f0d332729361e168efb9f0a3c7776db1b3f
DarkKay007 <omarshadow44@gmail.com>
2024-06-06 23:53:31 -0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:666', // Cambia al puerto donde se ejecuta tu servidor backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: "/documentacionGestor/",
 });
